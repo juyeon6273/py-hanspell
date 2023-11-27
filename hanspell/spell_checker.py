b3ac74c3117d37c23e00a16fcb44dea4223457dc
juyeon6273 <106322912+juyeon6273@users.noreply.github.com>
2023-11-28 05:15:11 +0900

    Update spell_checker.py

--- hanspell/spell_checker.py
+++ hanspell/spell_checker.py
@@ -7,6 +7,8 @@ import requests
 import json
 import time
 import sys
+import re
+from urllib import parse
 from collections import OrderedDict
 import xml.etree.ElementTree as ET
 
@@ -18,6 +20,24 @@ from .constants import CheckResult
 _agent = requests.Session()
 PY3 = sys.version_info[0] == 3
 
+def read_token():
+    with open("token.txt", "r") as f:
+        TOKEN = f.read()
+    return TOKEN
+
+def update_token(agent):
+    """update passportkey
+    from https://gist.github.com/AcrylicShrimp/4c94db38b7d2c4dd2e832a7d53654e42
+    """
+
+    html = agent.get(url='https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=맞춤법검사기') 
+
+    match = re.search('passportKey=([a-zA-Z0-9]+)', html.text)
+    if match is not None:
+        TOKEN = parse.unquote(match.group(1))
+        with open("token.txt", "w") as f:
+            f.write(TOKEN)
+    return TOKEN
 
 def _remove_tags(text):
     text = u'<content>{}</content>'.format(text).replace('<br>','')
@@ -27,6 +47,27 @@ def _remove_tags(text):
     result = ''.join(ET.fromstring(text).itertext())
 
     return result
+
+def get_response(TOKEN, text):
+    
+    payload = {
+        'passportKey' : TOKEN,
+        'q': text,
+        'color_blindness': 0
+    }
+    
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+        'referer': 'https://search.naver.com/',
+    }
+    
+    r = _agent.get(base_url, params=payload, headers=headers)
+    data = json.loads(r.text)
+    
+    if ('error' in data['message']) :
+        r = get_response(update_token(_agent), text)
+             
+    return r
 
 
 def check(text):
@@ -43,26 +84,14 @@ def check(text):
     # 최대 500자까지 가능.
     if len(text) > 500:
         return Checked(result=False)
-
-    payload = {
-        '_callback': 'jQuery11240003383472025177525_1680133565087',
-        'q': text,
-        'where': 'nexearch',
-        'color_blindness': 0
-    }
-
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
-        'referer': 'https://search.naver.com/',
-    }
-
+    
+    TOKEN = read_token()
+    
     start_time = time.time()
-    r = _agent.get(base_url, params=payload, headers=headers)
+    r = get_response(TOKEN, text)
     passed_time = time.time() - start_time
-
-    r = r.text[44:-2]
-
-    data = json.loads(r)
+    
+    data = json.loads(r.text)
     html = data['message']['result']['html']
     result = {
         'result': True,
@@ -76,11 +105,11 @@ def check(text):
     # 띄어쓰기로 구분하기 위해 태그는 일단 보기 쉽게 바꿔둠.
     # ElementTree의 iter()를 써서 더 좋게 할 수 있는 방법이 있지만
     # 이 짧은 코드에 굳이 그렇게 할 필요성이 없으므로 일단 문자열을 치환하는 방법으로 작성.
-    html = html.replace('<span class=\'green_text\'>', '<green>') \
-               .replace('<span class=\'red_text\'>', '<red>') \
-               .replace('<span class=\'purple_text\'>', '<purple>') \
-               .replace('<span class=\'blue_text\'>', '<blue>') \
-               .replace('</span>', '<end>')
+    html = html.replace('<em class=\'green_text\'>', '<green>') \
+               .replace('<em class=\'red_text\'>', '<red>') \
+               .replace('<em class=\'violet_text\'>', '<violet>') \
+               .replace('<em class=\'blue_text\'>', '<blue>') \
+               .replace('</em>', '<end>')
     items = html.split(' ')
     words = []
     tmp = ''
@@ -105,9 +134,9 @@ def check(text):
         elif word[:7] == '<green>':
             check_result = CheckResult.WRONG_SPACING
             word = word.replace('<green>', '')
-        elif word[:8] == '<purple>':
+        elif word[:8] == '<violet>':
             check_result = CheckResult.AMBIGUOUS
-            word = word.replace('<purple>', '')
+            word = word.replace('<violet>', '')
         elif word[:6] == '<blue>':
             check_result = CheckResult.STATISTICAL_CORRECTION
             word = word.replace('<blue>', '')
